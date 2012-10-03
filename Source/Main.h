312c06b5e4b49470a440216987b9384298643d49
jim <obs.jim@gmail.com>
2012-10-03 16:12:31 -0700

    0.411a - forgot version number

--- Source/Main.h
+++ Source/Main.h
@@ -62,7 +62,7 @@ extern OBS          *App;
 extern TCHAR        lpAppDataPath[MAX_PATH];
 
 #define OBS_VERSION             0x000410
-#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.41a"
+#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.411a"
 #define OBS_VERSION_STRING      TEXT(OBS_VERSION_STRING_ANSI)
 
 #define OBS_WINDOW_CLASS      TEXT("OBSWindowClass")
