2b0385b4811807f543bcf4c2c31e2eee37ba2a3f
jp9000 <obs.jim@gmail.com>
2013-03-14 23:41:03 -0700

    changed x264 version to 130

--- rundir/copydebug64.bat
+++ rundir/copydebug64.bat
@@ -5,6 +5,6 @@ copy ..\dshowplugin\x64\debug\dshowplugin.dll .\plugins
 copy ..\graphicscapture\x64\debug\graphicscapture.dll .\plugins
 copy ..\graphicscapture\graphicscapturehook\debug\graphicscapturehook.dll .\plugins\graphicscapture
 copy ..\graphicscapture\graphicscapturehook\x64\debug\graphicscapturehook64.dll .\plugins\graphicscapture
-copy ..\x264\libs\64bit\libx264-129.dll .\
+copy ..\x264\libs\64bit\libx264-130.dll .\
 copy ..\injectHelper\Release\injectHelper.exe .\plugins\graphicscapture
 copy ..\injectHelper\x64\Release\injectHelper64.exe .\plugins\graphicscapture
--- rundir/copyrelease.bat
+++ rundir/copyrelease.bat
@@ -5,6 +5,6 @@ copy ..\dshowplugin\release\dshowplugin.dll .\plugins
 copy ..\graphicscapture\release\graphicscapture.dll .\plugins
 copy ..\graphicscapture\graphicscapturehook\release\graphicscapturehook.dll .\plugins\graphicscapture
 copy ..\graphicscapture\graphicscapturehook\x64\release\graphicscapturehook64.dll .\plugins\graphicscapture
-copy ..\x264\libs\32bit\libx264-129.dll .\
+copy ..\x264\libs\32bit\libx264-130.dll .\
 copy ..\injectHelper\Release\injectHelper.exe .\plugins\graphicscapture
 copy ..\injectHelper\x64\Release\injectHelper64.exe .\plugins\graphicscapture
