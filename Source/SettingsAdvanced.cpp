10b43836bfb5a572a06bbd6dc7f96bd109833f8f
jp9000 <obs.jim@gmail.com>
2013-03-18 14:29:19 -0700

    changing the size of the scene buffering time temporarily - DON'T TEST ME KRAZY

--- Source/SettingsAdvanced.cpp
+++ Source/SettingsAdvanced.cpp
@@ -152,7 +152,7 @@ INT_PTR SettingsAdvanced::ProcMessage(UINT message, WPARAM wParam, LPARAM lParam
                 //------------------------------------
 
                 UINT sceneBufferingTime = GlobalConfig->GetInt(TEXT("General"), TEXT("SceneBufferingTime"), 400);
-                SendMessage(GetDlgItem(hwnd, IDC_SCENEBUFFERTIME), UDM_SETRANGE32, 100, 1000);
+                SendMessage(GetDlgItem(hwnd, IDC_SCENEBUFFERTIME), UDM_SETRANGE32, 60, 3000);
                 SendMessage(GetDlgItem(hwnd, IDC_SCENEBUFFERTIME), UDM_SETPOS32, 0, sceneBufferingTime);
 
                 //------------------------------------
