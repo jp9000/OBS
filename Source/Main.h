4664b8d266167f547ec8c10dde796ab321e65d51
jp9000 <obs.jim@gmail.com>
2013-04-21 11:39:36 -0700

    0.48.018 - updated french translation

--- Source/Main.h
+++ Source/Main.h
@@ -73,7 +73,7 @@ extern TCHAR        lpAppPath[MAX_PATH];
 extern TCHAR        lpAppDataPath[MAX_PATH];
 
 #define OBS_VERSION             0x000472
-#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.48.016b (the one ring test build)"
+#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.48.018b (rock lobster test build)"
 #define OBS_VERSION_STRING      TEXT(OBS_VERSION_STRING_ANSI)
 #define OBS_TEST_BUILD          1 //define this if releasing a test build to disable the auto updater
 
