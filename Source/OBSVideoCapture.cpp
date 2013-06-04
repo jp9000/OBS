0ca911d2f649def01e98324317231e9e5c5d98fb
palana <palana@stunned.de>
2013-06-05 00:25:58 +0200

    Make profiler output for lagged frames setting default depend on OBS_TEST_BUILD

--- Source/OBSVideoCapture.cpp
+++ Source/OBSVideoCapture.cpp
@@ -261,6 +261,11 @@ void STDCALL SleepTo(QWORD qw100NSTime)
 }
 #endif
 
+#ifdef OBS_TEST_BUILD
+#define LOGLONGFRAMESDEFAULT 1
+#else
+#define LOGLONGFRAMESDEFAULT 0
+#endif
 
 //todo: this function is an abomination, this is just disgusting.  fix it.
 //...seriously, this is really, really horrible.  I mean this is amazingly bad.
@@ -272,7 +277,7 @@ void OBS::MainCaptureLoop()
     bSentHeaders = false;
     bFirstAudioPacket = true;
 
-    bool bLogLongFramesProfile = GlobalConfig->GetInt(TEXT("General"), TEXT("LogLongFramesProfile"), 1) != 0;
+    bool bLogLongFramesProfile = GlobalConfig->GetInt(TEXT("General"), TEXT("LogLongFramesProfile"), LOGLONGFRAMESDEFAULT) != 0;
     float logLongFramesProfilePercentage = GlobalConfig->GetFloat(TEXT("General"), TEXT("LogLongFramesProfilePercentage"), 10.f);
 
     Vect2 baseSize    = Vect2(float(baseCX), float(baseCY));
