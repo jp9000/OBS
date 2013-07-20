c92f11bbf951168cc9b33e5e81dd16d144acfe50
shousa <gatoh.solomon@gmail.com>
2013-07-20 11:36:05 +0900

    Change commited files from binary to text
    Again add them.

---
+++ PSVPlugin/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PSVPlugin.rc
+//
+#define IDD_CONFIGPSV                   101
+#define IDC_CHECK1                      1001
+#define IDC_TOGGLEPSV                   1001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
