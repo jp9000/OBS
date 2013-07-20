a16543051b49095e374441476187a37245b9dd18
jp9000 <obs.jim@gmail.com>
2013-07-19 22:34:50 -0700

    updated x264 to the latest development branch

--- x264/x264_config.h
+++ x264/x264_config.h
@@ -1,8 +1,8 @@
 #define X264_BIT_DEPTH     8
 #define X264_GPL           1
-#define X264_INTERLACED    1
+#define X264_INTERLACED    0
 #define X264_CHROMA_FORMAT 0
-#define X264_REV 2348
+#define X264_REV 2350
 #define X264_REV_DIFF 0
-#define X264_VERSION " r2348 c1161c6"
-#define X264_POINTVER "0.136.2348 c1161c6"
+#define X264_VERSION " r2350 dba1934"
+#define X264_POINTVER "0.136.2350 dba1934"
