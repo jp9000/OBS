1b7d01985e56a3445f0fad4f939f5ce42380c7ec
Richard Stanway <r.stanway@gmail.com>
2013-07-21 03:58:24 -0400

    Actually resolve the hostname before logging interface info

--- Source/RTMPPublisher.cpp
+++ Source/RTMPPublisher.cpp
@@ -570,8 +570,19 @@ void LogInterfaceType (RTMP *rtmp)
 {
     MIB_IPFORWARDROW    route;
     DWORD               destAddr;
+    CHAR               hostname[256];
 
-    destAddr = inet_addr(rtmp->Link.hostname.av_val);
+    if (rtmp->Link.hostname.av_len >= sizeof(hostname)-1)
+        return;
+
+    strncpy (hostname, rtmp->Link.hostname.av_val, sizeof(hostname)-1);
+    hostname[rtmp->Link.hostname.av_len] = 0;
+
+    HOSTENT *h = gethostbyname(hostname);
+    if (!h)
+        return;
+
+    destAddr = *(DWORD *)h->h_addr_list[0];
 
     if (!GetBestRoute (destAddr, rtmp->m_bindIP.addr.sin_addr.S_un.S_addr, &route))
     {
