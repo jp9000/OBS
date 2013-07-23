20fa04accdbf1a51f2ac0ff649f8f786e8986779
jp9000 <obs.jim@gmail.com>
2013-07-23 05:58:57 -0700

    allow negative audio offset for directshow devices to be up to 3 seconds

--- Source/Main.h
+++ Source/Main.h
@@ -73,8 +73,8 @@ extern bool         bIsPortable;
 extern TCHAR        lpAppPath[MAX_PATH];
 extern TCHAR        lpAppDataPath[MAX_PATH];
 
-#define OBS_VERSION             0x005403 //version number is 0xMMmmtt (Major.minor.test - yes I know it's hex)
-#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.54.03 network test"
+#define OBS_VERSION             0x005404 //version number is 0xMMmmtt (Major.minor.test - yes I know it's hex)
+#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.54.04 network test"
 #define OBS_VERSION_STRING      TEXT(OBS_VERSION_STRING_ANSI)
 #define OBS_TEST_BUILD          1 //define this if releasing a test build to disable the auto updater
 
