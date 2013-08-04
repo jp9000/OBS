14cdd9c17d86a456fb88d81239b1dd9fac788b21
jp9000 <obs.jim@gmail.com>
2013-08-03 22:28:13 -0700

    updated version to 0.54.07

--- Source/Main.h
+++ Source/Main.h
@@ -73,8 +73,8 @@ extern bool         bIsPortable;
 extern TCHAR        lpAppPath[MAX_PATH];
 extern TCHAR        lpAppDataPath[MAX_PATH];
 
-#define OBS_VERSION             0x005406 //version number is 0xMMmmtt (Major.minor.test - yes I know it's hex)
-#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.54.06 network/gc test"
+#define OBS_VERSION             0x005407 //version number is 0xMMmmtt (Major.minor.test - yes I know it's hex)
+#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.54.07 network/gc test"
 #define OBS_VERSION_STRING      TEXT(OBS_VERSION_STRING_ANSI)
 #define OBS_TEST_BUILD          1 //define this if releasing a test build to disable the auto updater
 
