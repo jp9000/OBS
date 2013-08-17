b064ddf49338a7d1e7061fe833288b091b6031c2
jp9000 <obs.jim@gmail.com>
2013-08-16 17:38:56 -0700

    remove test build flag

--- Source/Main.h
+++ Source/Main.h
@@ -76,7 +76,7 @@ extern TCHAR        lpAppDataPath[MAX_PATH];
 #define OBS_VERSION             0x005501 //version number is 0xMMmmtt (Major.minor.test - yes I know it's hex)
 #define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.551b"
 #define OBS_VERSION_STRING      TEXT(OBS_VERSION_STRING_ANSI)
-#define OBS_TEST_BUILD          1 //define this if releasing a test build to disable the auto updater
+//#define OBS_TEST_BUILD          1 //define this if releasing a test build to disable the auto updater
 
 #ifdef _DEBUG
 #define OBS_DISABLE_AUTOUPDATE 1
