96356a657f3317bb13a8aba3e6f504c0a6711065
jp9000 <obs.jim@gmail.com>
2013-08-17 12:01:22 -0700

    ...removed "bla version" from the version string

--- Source/Main.h
+++ Source/Main.h
@@ -74,7 +74,7 @@ extern TCHAR        lpAppPath[MAX_PATH];
 extern TCHAR        lpAppDataPath[MAX_PATH];
 
 #define OBS_VERSION             0x005502 //version number is 0xMMmmtt (Major.minor.test - yes I know it's hex)
-#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.552b bla version"
+#define OBS_VERSION_STRING_ANSI "Open Broadcaster Software v0.552b"
 #define OBS_VERSION_STRING      TEXT(OBS_VERSION_STRING_ANSI)
 //#define OBS_TEST_BUILD          1 //define this if releasing a test build to disable the auto updater
 
