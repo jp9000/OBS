ab5e0ff4850c8c1e57e75499c1d1f5d0a4380f79
jp9000 <obs.jim@gmail.com>
2013-09-03 09:25:19 -0700

    set lower fps limit to 1 fps

--- Source/SettingsVideo.cpp
+++ Source/SettingsVideo.cpp
@@ -351,7 +351,7 @@ INT_PTR SettingsVideo::ProcMessage(UINT message, WPARAM wParam, LPARAM lParam)
                 int topFPS = bUnlockFPS ? 120 : 60;
 
                 hwndTemp = GetDlgItem(hwnd, IDC_FPS);
-                SendMessage(hwndTemp, UDM_SETRANGE32, 10, topFPS);
+                SendMessage(hwndTemp, UDM_SETRANGE32, 1, topFPS);
 
                 int fps = AppConfig->GetInt(TEXT("Video"), TEXT("FPS"), 30);
                 if(!AppConfig->HasKey(TEXT("Video"), TEXT("FPS")))
