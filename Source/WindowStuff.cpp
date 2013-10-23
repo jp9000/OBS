jp9000/OBS

commit 1cb6bd3fbd7f7193f3e1f7687f020d0edfd01aa8

Fixed source properties window while not previewing

Source/WindowStuff.cpp
@@ -844,36 +844,29 @@ void OBS::TrackModifyListbox(HWND hwnd, int ret)
 
         case ID_LISTBOX_CONFIG:
             {
-                Vect2 multiple;
-                ImageSource *source;
-
                 App->EnableSceneSwitching(false);
 
-                List<SceneItem*> selectedSceneItems;
-                if(App->scene)
-                    App->scene->GetSelectedItems(selectedSceneItems);
-
-                if (!selectedSceneItems.Num())
-                {
-                    App->EnableSceneSwitching(true);
-                    break;
-                }
-
-                if(App->bRunning)
-                {
-                    source = selectedSceneItems[0]->GetSource();
-                    if(source)
-                    {
-                        Vect2 curSize = Vect2(selectedElement->GetFloat(TEXT("cx"), 32.0f), selectedElement->GetFloat(TEXT("cy"), 32.0f));
-                        Vect2 baseSize = source->GetSize();
-
-                        multiple = curSize/baseSize;
-                    }
-                }
                 if(curClassInfo->configProc && curClassInfo->configProc(selectedElement, false))
                 {
-                    if(App->bRunning)
+                    List<SceneItem*> selectedSceneItems;
+                    if(App->scene)
+                        App->scene->GetSelectedItems(selectedSceneItems);
+
+                    if(App->bRunning && selectedSceneItems.Num())
                     {
+                        Vect2 multiple;
+                        ImageSource *source;
+
+                        source = selectedSceneItems[0]->GetSource();
+
+                        if(source)
+                        {
+                            Vect2 curSize = Vect2(selectedElement->GetFloat(TEXT("cx"), 32.0f), selectedElement->GetFloat(TEXT("cy"), 32.0f));
+                            Vect2 baseSize = source->GetSize();
+
+                            multiple = curSize/baseSize;
+                        }
+
                         App->EnterSceneMutex();
 
                         if(source)

commit 5e1bb56c9eb02bd309b07a8c122b3b4d708d432b

Fixed a bug introduced in 1cb6bd3fbd7f7193f3e1f7687f020d0edfd01aa8 , calc scale before config

Source/WindowStuff.cpp
@@ -846,27 +846,31 @@ void OBS::TrackModifyListbox(HWND hwnd, int ret)
             {
                 App->EnableSceneSwitching(false);
 
+                List<SceneItem*> selectedSceneItems;
+
+                if(App->scene)
+                    App->scene->GetSelectedItems(selectedSceneItems);
+		
+                ImageSource *source = NULL;
+                Vect2 multiple;
+
+                if(App->bRunning && selectedSceneItems.Num())
+                {
+                    source = selectedSceneItems[0]->GetSource();
+                    if(source)
+                    {
+                        Vect2 curSize = Vect2(selectedElement->GetFloat(TEXT("cx"), 32.0f), selectedElement->GetFloat(TEXT("cy"), 32.0f));
+                        Vect2 baseSize = source->GetSize();
+
+                        multiple = curSize/baseSize;
+                    }
+                }
+
                 if(curClassInfo && curClassInfo->configProc && curClassInfo->configProc(selectedElement, false))
                 {
-                    List<SceneItem*> selectedSceneItems;
-                    if(App->scene)
-                        App->scene->GetSelectedItems(selectedSceneItems);
 
                     if(App->bRunning && selectedSceneItems.Num())
                     {
-                        Vect2 multiple;
-                        ImageSource *source;
-
-                        source = selectedSceneItems[0]->GetSource();
-
-                        if(source)
-                        {
-                            Vect2 curSize = Vect2(selectedElement->GetFloat(TEXT("cx"), 32.0f), selectedElement->GetFloat(TEXT("cy"), 32.0f));
-                            Vect2 baseSize = source->GetSize();
-
-                            multiple = curSize/baseSize;
-                        }
-
                         App->EnterSceneMutex();
 
                         if(source)