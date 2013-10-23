jp9000/OBS

commit 78cfebc527c08a186f39224e0ccb0d20ef0f6204

Show main wnd after we're done with init stuff and not before

Source/OBS.cpp
@@ -532,8 +532,7 @@ OBS::OBS()
     API = CreateOBSApiInterface();
 
     bDragResize = false;
-    ResizeWindow(false);
-    ShowWindow(hwndMain, SW_SHOW);
+
     if(GlobalConfig->GetInt(TEXT("General"), TEXT("Maximized")))
     { // Window was maximized last session
         SendMessage(hwndMain, WM_SYSCOMMAND, SC_MAXIMIZE, 0);
@@ -679,6 +678,10 @@ OBS::OBS()
 
         OSFindClose(hFind);
     }
+
+    ResizeWindow(false);
+    ShowWindow(hwndMain, SW_SHOW);
+
 }
 
 

commit be5e859c4dbfef53384ead251d16bde04123837b

Fixed a small UI bug introduced by 78cfebc (sources listview columns not being resized properly)

Source/OBS.cpp
@@ -538,11 +538,6 @@ OBS::OBS()
         SendMessage(hwndMain, WM_SYSCOMMAND, SC_MAXIMIZE, 0);
     }
 
-    // make sure sources listview column widths are as expected after obs window is shown
-
-    ListView_SetColumnWidth(hwndSources,0,LVSCW_AUTOSIZE_USEHEADER);
-    ListView_SetColumnWidth(hwndSources,1,LVSCW_AUTOSIZE_USEHEADER);
-
     //-----------------------------------------------------
 
     for(UINT i=0; i<numScenes; i++)
@@ -681,6 +676,11 @@ OBS::OBS()
 
     ResizeWindow(false);
     ShowWindow(hwndMain, SW_SHOW);
+
+    // make sure sources listview column widths are as expected after obs window is shown
+
+    ListView_SetColumnWidth(hwndSources,0,LVSCW_AUTOSIZE_USEHEADER);
+    ListView_SetColumnWidth(hwndSources,1,LVSCW_AUTOSIZE_USEHEADER);
 
     if (bStreamOnStart)
         PostMessage(hwndMain, WM_COMMAND, MAKEWPARAM(ID_STARTSTOP, 0), NULL);