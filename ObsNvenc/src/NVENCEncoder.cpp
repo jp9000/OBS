2b3079c88442472b7a5bef29e90a646a55e1a692
BtbN <btbn@btbn.de>
2014-01-10 02:32:37 +0100

    Change nvenc preset back to HQ, since so many people hit the performance limits with llhq.

--- ObsNvenc/src/NVENCEncoder.cpp
+++ ObsNvenc/src/NVENCEncoder.cpp
@@ -145,7 +145,7 @@ void NVENCEncoder::init()
     NVENCSTATUS nvStatus = NV_ENC_SUCCESS;
     int surfaceCount = 0;
 
-    GUID encoderPreset = NV_ENC_PRESET_LOW_LATENCY_HQ_GUID;
+    GUID encoderPreset = NV_ENC_PRESET_HQ_GUID;
 
     String profileString = AppConfig->GetString(TEXT("Video Encoding"), TEXT("X264Profile"), TEXT("high"));
 
