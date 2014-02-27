693ec14bee22818a27552d2b9745cdcb18abfc60
palana <palana@stunned.de>
2014-02-27 17:45:29 +0100

    Remove 'Set base resolution' button from DesktopImageSource config dialogs
    Reasons for removal:
    - there was no feedback when clicking the button
    - missing confirmation/undo for such a fundamental config change

--- Source/DesktopImageSource.cpp
+++ Source/DesktopImageSource.cpp
@@ -1710,29 +1710,6 @@ INT_PTR CALLBACK ConfigDesktopSourceProc(HWND hwnd, UINT message, WPARAM wParam,
                         break;
                     }
 
-                case IDC_SETSTREAMSIZE:
-                    {
-                        UINT sizeX = (UINT)GetEditText(GetDlgItem(hwnd, IDC_SIZEX)).ToInt();
-                        UINT sizeY = (UINT)GetEditText(GetDlgItem(hwnd, IDC_SIZEY)).ToInt();
-
-                        if(sizeX < 128)
-                            sizeX = 128;
-                        else if(sizeX > 4096)
-                            sizeX = 4096;
-
-                        if(sizeY < 128)
-                            sizeY = 128;
-                        else if(sizeY > 4096)
-                            sizeY = 4096;
-
-                        AppConfig->SetInt(TEXT("Video"), TEXT("BaseWidth"), sizeX);
-                        AppConfig->SetInt(TEXT("Video"), TEXT("BaseHeight"), sizeY);
-
-                        if(!App->IsRunning())
-                            App->ResizeWindow(false);
-                    }
-                    break;
-
                 case IDC_MONITOR:
                     {
                         UINT id = (UINT)SendMessage(GetDlgItem(hwnd, IDC_MONITOR), CB_GETCURSEL, 0, 0);
