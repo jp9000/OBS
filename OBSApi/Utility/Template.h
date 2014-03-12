f0bf74e422d61d4036e7c7388bd85c95c75c20da
palana <palana@stunned.de>
2014-03-12 15:41:31 +0100

    Remove warning disabling pragma and properly mark methods as deleted

--- OBSApi/Utility/Template.h
+++ OBSApi/Utility/Template.h
@@ -19,15 +19,13 @@
 
 #pragma once
 
-#pragma warning(disable: 4661)
-
 template<typename T> class List
 {
 private:
-    List(const List&)/* = delete*/;
-    List(List&&)/* = delete*/;
-    List& operator=(const List&)/* = delete*/;
-    List& operator=(List&&)/* = delete*/;
+    List(List const&) = delete;
+    List(List&&) = delete;
+    List &operator=(List const&) = delete;
+    List &operator=(List&&) = delete;
 protected:
     T *array;
     unsigned int num;
