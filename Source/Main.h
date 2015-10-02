809d7aff67b2635b33798a1a26f6a2a8e2d9a3cf
jp9000 <obs.jim@gmail.com>
2015-10-02 15:15:19 -0700

    Update to version 0.656

--- Source/Main.h
+++ Source/Main.h
@@ -81,8 +81,8 @@ extern TCHAR        lpAppDataPath[MAX_PATH];
 #define OBS_VERSION_SUFFIX ""
 #endif
 
-#define OBS_VERSION             0x006505 //version number is 0xMMmmtt (super-major.major.minor - hex)
-#define OBS_VERSION_STRING_RAW  "Open Broadcaster Software v0.655b"
+#define OBS_VERSION             0x006506 //version number is 0xMMmmtt (super-major.major.minor - hex)
+#define OBS_VERSION_STRING_RAW  "Open Broadcaster Software v0.656b"
 //#define OBS_TEST_BUILD          1 //define this if releasing a test build to disable the auto updater
 
 #define OBS_VERSION_STRING_ANSI OBS_VERSION_STRING_RAW OBS_VERSION_SUFFIX
